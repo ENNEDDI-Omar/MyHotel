16fa546c3207a888f9382d8fea1a945cd93982bd
ENNEDDI-Omar <enneddiomar@gmail.com>
2024-08-30 23:12:21 +0200

    mise en place des sous Menus

--- src/Main.java
+++ src/Main.java
@@ -48,10 +48,102 @@ public class Main {
                 default:
                     System.out.println("Choix d'option Invalide, Veuillez réessayer.");
 
-
-
             }
         }
+      sc.close();
+    }
 
+    private static void afficherSousMenuInformations(Scanner sc, Client c1, Client c2, Client c3, Client c4, Chambre ch1, Chambre ch2, Chambre ch3, Chambre ch4)
+    {
+        boolean retourVersMenuPrincipale = false;
+
+        while (!retourVersMenuPrincipale)
+        {
+            //affichage du sous menu d'informations:
+            System.out.println("\nMenu d'Informations : ");
+            System.out.println("1 - Afficher les Informations d'un seul Client :");
+            System.out.println("2 - Lister les Clients :");
+            System.out.println("3 - Vérifier une Chambre :");
+            System.out.println("4 - Lister toutes les Chambres :");
+            System.out.println("5 - Info sur une Réservation :");
+            System.out.println("6 - Lister toutes les Réservations");
+            System.out.println("7 - Retour vers le Menu Principal :");
+
+            System.out.println("Veuillez choisir une option: ");
+            int optionMenuInformation = sc.nextInt();
+
+            switch (optionMenuInformation)
+            {
+                case 1:
+                    //Afficher les Informations d'un seul Client
+
+                    break;
+                case 2:
+                    //Lister les Clients
+
+                    break;
+                case 3:
+                    //Vérifier une Chambre
+
+                    break;
+                case 4:
+                    //Lister toutes les Chambres
+
+                    break;
+                case 5:
+                    //Info sur une Réservation
+
+                    break;
+                case 6:
+                    //Lister toutes les Réservations
+
+                    break;
+                case 7:
+                    //Retour vers le Menu Principal
+                     retourVersMenuPrincipale = true;
+                    break;
+                default:
+                    System.out.println("Option invalide. Veuillez réessayer.");
+            }
+        }
+    }
+
+    private static void afficherSousMenuReservations(Scanner sc, Client c1, Client c2, Client c3, Client c4, Chambre ch1, Chambre ch2, Chambre ch3, Chambre ch4)
+    {
+         boolean retourVersMenuPrincipale = false;
+
+         while (!retourVersMenuPrincipale)
+         {
+             System.out.println("\nMenu d'Reservations : ");
+             System.out.println("1 - Faire une Réservation:");
+             System.out.println("2 - Modifier une Réservation:");
+             System.out.println("3 - Annuler une Réservation: ");
+             System.out.println("4 - Retour vers le Menu Principal :");
+
+             System.out.println("Option invalide. Veuillez réessayer.");
+             int optionMenuReservation = sc.nextInt();
+
+             switch (optionMenuReservation)
+             {
+                 case 1:
+                     //Faire une Réservation
+
+                     break;
+                 case 2:
+                     //Modifier une Réservation
+
+                     break;
+                 case 3:
+                     //Annuler une Réservation
+
+                     break;
+                 case 4:
+                     //Retour vers le Menu Principal
+                     retourVersMenuPrincipale = true;
+                     break;
+                 default:
+                     System.out.println("Option Invalide. Veuillez réessayer.");
+             }
+         }
     }
 }
